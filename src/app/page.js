e3b68cb80592fa5087a70f994e13191d9c08557a
Arnav Agarwal <137907741+Arnav-Agarwal-01@users.noreply.github.com>
2025-11-28 02:37:11 +0530

    added analytics

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import LandingPage from "@/components/landingpagecomponent";
 
+
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black overflow-hidden">
